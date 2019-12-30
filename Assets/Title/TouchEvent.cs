b3ae02e01ff8dfe918631e2717950c4e2030dd65
LingFeng-bbben <bengy@126.com>
2019-12-31 06:24:54 +0800

    title improved

---
+++ Assets/Title/TouchEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TouchEvent : MonoBehaviour
+{
+    public void SwitchScene(int i)
+    {
+        SceneManager.LoadSceneAsync(i);
+    }
+    public void ChangeAnimation()
+    {
+        GameObject.Find("Main Camera").GetComponent<Animator>().SetBool("Touched", true);
+        StartCoroutine(delayAndSwitch());
+    }
+    IEnumerator delayAndSwitch()
+    {
+        yield return new WaitForSeconds(2f);
+        SwitchScene(1);
+    }
+}
