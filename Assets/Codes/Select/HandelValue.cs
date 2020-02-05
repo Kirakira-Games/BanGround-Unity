91527d0ce1c6e4f64e8715b199ef1bc09ad50365
Frenqy <411937015@qq.com>
2020-02-05 16:21:39 +0800

    Finished setting panel

---
+++ Assets/Codes/Select/HandelValue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandelValue : MonoBehaviour
+{
+    private Slider slider;
+    private Text valueText;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        valueText = transform.Find("Handle Slide Area/Handle/Value").GetComponent<Text>();
+
+        valueText.text = ((int)(slider.value * 100)).ToString() + "%";
+        slider.onValueChanged.AddListener((value) =>
+        {
+            valueText.text = ((int)(value * 100)).ToString() + "%";
+        });
+    }
+
+}
---
+++ Assets/Codes/Select/SwitchToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class SwitchToggle : MonoBehaviour
+{
+    private Toggle toggle;
+    private GameObject panelObject;
+    [SerializeField] private string panel;
+    [SerializeField] private bool init = false;
+
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+        panelObject = GameObject.Find(panel);
+        toggle.onValueChanged.AddListener((active) =>
+        {
+            panelObject.SetActive(active);
+        });
+
+        panelObject.SetActive(init);
+    }
+}
