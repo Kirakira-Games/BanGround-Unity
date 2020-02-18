e941981be2872abe599c0bf3463904752cd6b898
Frenqy <411937015@qq.com>
2020-02-18 23:27:49 +0800

    Fix Android Importer(?) / update version text

--- Assets/Plugins/Android/KirakiraActivity.java
+++ Assets/Plugins/Android/KirakiraActivity.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.unity3d.player.UnityPlayerActivity;
 
@@ -23,42 +28,7 @@ public class KirakiraActivity extends UnityPlayerActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        while(checkSelfPermission("android.permission.WRITE_EXTERNAL_STORAGE") != PackageManager.PERMISSION_GRANTED)
-        {
-            requestPermissions(PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
-        }
-
-        Intent intent = getIntent();
-        String action = intent.getAction();
-
-        assert action != null;
-        if (action.compareTo("android.intent.action.VIEW") == 0) {
-            String scheme = intent.getScheme();
-            ContentResolver resolver = getContentResolver();
-
-            assert scheme != null;
-            if (scheme.compareTo("file") == 0) {
-                InputStream input;
-                Uri uri = intent.getData();
-                String name = uri.getLastPathSegment();
-
-                Log.v("tag", "File intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
-
-                try {
-                    input = resolver.openInputStream(uri);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    return;
-                }
-
-                File inbox = new File(getAndroidStorageFile(), "/Inbox/");
-                if(!inbox.exists())
-                    inbox.mkdir();
-
-                InputStreamToFile(input, new File(inbox, name));
-            }
-        }
+        ImportFile();
     }
 
     private void InputStreamToFile(InputStream in, File file) {
@@ -76,8 +46,69 @@ public class KirakiraActivity extends UnityPlayerActivity {
         }
     }
 
+    public void ImportFile(){
+        while(checkSelfPermission("android.permission.WRITE_EXTERNAL_STORAGE") != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+        }
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+
+        assert action != null;
+        if (action.compareTo("android.intent.action.VIEW") == 0) {
+            String scheme = intent.getScheme();
+            ContentResolver resolver = getContentResolver();
+
+            assert scheme != null;
+            if (scheme.compareTo("file") == 0 || scheme.compareTo("content") == 0) {
+                InputStream input;
+                Uri uri = intent.getData();
+                String name = uri.getLastPathSegment();
+
+                Log.v("tag", "File intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
+
+                try {
+                    input = resolver.openInputStream(uri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    return;
+                }
+
+                File inbox = new File(getAndroidStorageFile(), "/Inbox/");
+                Log.v("Unity",inbox.getAbsolutePath());
+                if(!inbox.exists())
+                    inbox.mkdir();
+
+                InputStreamToFile(input, new File(inbox, name));
+            }
+        }
+    }
 
     public File getAndroidStorageFile() {
-        return this.getFilesDir();
+        return this.getExternalFilesDir(null);
+    }
+
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus){
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // 沉浸 SYSTEM_UI_FLAG_IMMERSIVE.
+        // 粘性沉浸 SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // 全屏 隐藏导航栏 隐藏状态栏
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
