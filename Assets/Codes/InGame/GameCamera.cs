0a42ca3dac615d0c99c205cd11bb4bc0f1cc11cb
LingFeng-bbben <bengy@126.com>
2020-02-23 17:27:53 +0800

    fix aspect

--- Assets/Codes/InGame/GameCamera.cs
+++ Assets/Codes/InGame/GameCamera.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameCamera : MonoBehaviour
 {
+    /*
     void Awake()
     {
 
@@ -11,5 +12,14 @@ public class GameCamera : MonoBehaviour
         float size = aspectRatio >= 16f/9 ? 5 : (-3.9375f * aspectRatio + 12f);
 
         GetComponent<Camera>().orthographicSize = size;
+    }
+    */
+
+    private void Awake()
+    {
+        float Ratio = GetComponent<Camera>().aspect;
+        float size = Ratio >= 16f/9 ?60f:(-33.3f*Ratio+119.3f );
+        // (1,86) (1.78,60) 0.78 -26
+        GetComponent<Camera>().fieldOfView = size;
     }
 }
