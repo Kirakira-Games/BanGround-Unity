2bc3c5afd6223ff6b1dbe318cb3ab9302f525b8e
KCFindstr <changyuz@usc.edu>
2020-03-01 15:51:49 -0800

    Speed up!

--- Assets/Codes/LiveSettingConfiguration.cs
+++ Assets/Codes/LiveSettingConfiguration.cs
@@ -111,7 +111,7 @@ public static class LiveSetting
             if(cachedSpeed != trueNoteSpeed)
             {
                 cachedSpeed = trueNoteSpeed;
-                cachedScreenTime = (int)(-500 * trueNoteSpeed + 6500);
+                cachedScreenTime = (int)(-540 * trueNoteSpeed + 6500);
             }
 
             return (int)(cachedScreenTime * SpeedCompensationSum);
