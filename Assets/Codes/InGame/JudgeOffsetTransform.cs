3028c2b6af4928077695a7be7d4d689b566f5195
LingFeng-bbben <bengy@126.com>
2020-03-02 15:03:59 +0800

    movable judge result

---
+++ Assets/Codes/InGame/JudgeOffsetTransform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JudgeOffsetTransform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = new Vector3(0, LiveSetting.offsetTransform, 0);
+    }
+
+}
