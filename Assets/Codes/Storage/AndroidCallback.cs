281f729b9b8293b96e76f9ff405d161b97168e76
KCFindstr <changyuz@usc.edu>
2020-03-05 23:48:18 -0800

    Fix Android file import bug

--- Assets/Codes/Storage/AndroidCallback.cs
+++ Assets/Codes/Storage/AndroidCallback.cs
@@ -6,7 +6,16 @@ using System.Collections;
 public class AndroidCallback : AndroidJavaProxy
 {
     public AndroidCallback() : base("fun.banground.game.FileImportCallback") { }
-    
+
+    public static AndroidCallback instance;
+
+    public static void Init()
+    {
+        instance = new AndroidCallback();
+        AndroidJavaClass pluginClass = new AndroidJavaClass("fun.banground.game.KirakiraActivity");
+        pluginClass.CallStatic("registerFileImportCallback", instance);
+    }
+
     public void onFileImport()
     {
         if (DataLoader.LoadAllKiraPackFromInbox())
