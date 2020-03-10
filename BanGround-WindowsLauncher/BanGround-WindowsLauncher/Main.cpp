d55aa001b5fdad0636d819e8dc9a416a7dbe34f1
GEEKiDoS <geek_ds@foxmail.com>
2020-03-10 12:03:23 +0800

    Add Windows Launcher for importing stuff

---
+++ BanGround-WindowsLauncher/BanGround-WindowsLauncher/Main.cpp
@@ -0,0 +1,23 @@
+#include "stdinclude.hpp"
+
+extern "C"
+{
+    __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+{
+    CreateDebugConsole();
+    InstallFileType();
+    TryCopyKirapack();
+    
+    auto handle = FindWindow(L"UnityWndClass", L"BanGround-Unity");
+    if (handle != nullptr)
+    {
+        SetForegroundWindow(handle);
+        return 0;
+    }
+
+    return UnityMain(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+}
