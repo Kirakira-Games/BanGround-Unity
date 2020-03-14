2fb9b6c553d0c6cab635d5b978d44fe3a81b49f9
Frenqy <411937015@qq.com>
2020-03-14 10:03:13 +0800

    note rotate

--- Assets/Codes/InGame/Notes/NoteRotation.cs
+++ Assets/Codes/InGame/Notes/NoteRotation.cs
@@ -21,7 +21,10 @@ public class NoteRotation : MonoBehaviour
             //transform.rotation = Quaternion.Euler(800f / (transform.localPosition.x + 27f) - 54f + Mathf.Pow(2f, 0.024f * transform.localPosition.x), 0f, 0f);
             Vector3 dir = new Vector3(0, mainCamY, mainCamZ - transform.position.z);//new Vector3(transform.position.x, mainCamY, mainCamZ) - transform.position;
             float abs = Mathf.Abs(dir.z);
-            if (abs > 8) abs = 8;
+            Debug.Log(abs);
+            if (abs > 30) abs = 8;
+            else if (abs < 15) abs = 30;
+            else abs = 150 / (2 * abs - 24.2f) + 4;
             dir.z = abs;
             transform.rotation = Quaternion.LookRotation(dir);
         }
