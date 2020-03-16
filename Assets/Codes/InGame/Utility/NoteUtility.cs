723c6bc6206d648de2b314bedc42217bbbd471b2
KCFindstr <changyuz@usc.edu>
2020-03-16 15:15:56 -0700

    Clean up

--- Assets/Codes/InGame/Utility/NoteUtility.cs
+++ Assets/Codes/InGame/Utility/NoteUtility.cs
@@ -128,9 +128,9 @@ public static class NoteUtility
     public const float NOTE_Y_MAX = 4f;
     public const float LANE_WIDTH = 2f;
     public const float LANE_JUDGE_WIDTH = 2.5f;
-    public const float LANE_JUDGE_HEIGHT = 3f;
+    public const float LANE_JUDGE_HEIGHT = 2.5f;
     public const float NOTE_SCALE = 1.2f;
-    public const float FUWAFUWA_RADIUS = 3f;
+    public const float FUWAFUWA_RADIUS = 2.5f;
     public static Plane JudgePlane;
 
     private static readonly float BANG_PERSPECTIVE_START = YTo3DXHelper(0);
