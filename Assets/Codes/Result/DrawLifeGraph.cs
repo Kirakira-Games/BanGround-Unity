1f4aed43531f53b530971270f143a34b29a29af8
LingFeng-bbben <bengy@126.com>
2020-03-21 08:34:23 +0800

    血量统计条

---
+++ Assets/Codes/Result/DrawLifeGraph.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawLifeGraph : MonoBehaviour
+{
+    public Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DrawLines());
+    }
+    
+    IEnumerator DrawLines()
+    {
+        yield return new WaitForSeconds(2f);
+        for(int i = 0;i<LifeController.lifePerSecond.Count;i++)
+        {
+            System.Type[] compoints = new System.Type[] { typeof(RectTransform), typeof(Image) };
+            GameObject gmobj = Instantiate(new GameObject(i + " value", compoints),transform);
+            gmobj.GetComponent<RectTransform>().sizeDelta = new Vector2(1, 2 * LifeController.lifePerSecond[i]);
+            gmobj.GetComponent<Image>().color = color;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
