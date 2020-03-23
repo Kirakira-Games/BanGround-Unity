645e4bae97d2d8abd1004e6915cc1ac280af24a7
KCFindstr <changyuz@usc.edu>
2020-03-22 18:13:39 -0700

    Add KirakiraWebRequest

--- Assets/Codes/Web/Authenticate.cs
+++ Assets/Codes/Web/Authenticate.cs
@@ -15,29 +15,21 @@ public class Authenticate
 
     public static IEnumerator TryAuthenticate(string key, string uuid)
     {
-        var req = new AuthRequest { key = key, uuid = uuid };
-        var reqJson = JsonConvert.SerializeObject(req);
-        using (UnityWebRequest webRequest = new UnityWebRequest(FullAPI, "POST"))
+        var req = new KirakiraWebRequest<AuthResponse>();
+        var body = new AuthRequest { key = key, uuid = uuid };
+        yield return req.Post(FullAPI, body);
+        isNetworkError = req.isNetworkError;
+        if (req.isNetworkError)
         {
-            byte[] jsonToSend = new UTF8Encoding().GetBytes(reqJson);
-            webRequest.uploadHandler = new UploadHandlerRaw(jsonToSend);
-            webRequest.downloadHandler = new DownloadHandlerBuffer();
-            webRequest.SetRequestHeader("Content-Type", "application/json");
-            yield return webRequest.SendWebRequest();
-            if (webRequest.isNetworkError | webRequest.isHttpError)
+            result = new AuthResponse
             {
-                result = new AuthResponse
-                {
-                    status = false,
-                    error = "Network error"
-                };
-                isNetworkError = true;
-            }
-            else
-            {
-                result = JsonConvert.DeserializeObject<AuthResponse>(webRequest.downloadHandler.text);
-                isNetworkError = false;
-            }
+                status = false,
+                error = "Network error"
+            };
+        }
+        else
+        {
+            result = req.resp;
         }
     }
 }
