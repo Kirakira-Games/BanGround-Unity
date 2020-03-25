90251cf53af29a51dd719a38544e16a579ae7668
LingFeng-bbben <bengy@126.com>
2020-03-25 11:00:14 +0800

    转转

--- Assets/Codes/Select/SelectManager.cs
+++ Assets/Codes/Select/SelectManager.cs
@@ -96,6 +96,11 @@ public class SelectManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Screen.orientation = ScreenOrientation.AutoRotation;
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+        MessageBoxController.ShowMsg(LogLevel.INFO, Screen.orientation.ToString());
+
         InitComponent();
         LoadScoreRecord();
         InitSongList(false);
