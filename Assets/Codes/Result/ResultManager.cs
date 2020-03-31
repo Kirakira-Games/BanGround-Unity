a1ff327f625ec1d10560a8c5b42ea14a9e5a70b4
Frenqy <411937015@qq.com>
2020-03-31 15:12:51 +0800

    ACC展示时使用去尾法

--- Assets/Codes/Result/ResultManager.cs
+++ Assets/Codes/Result/ResultManager.cs
@@ -322,7 +322,7 @@ public class ResultManager : MonoBehaviour
         level_Text.text = Enum.GetName(typeof(Difficulty), LiveSetting.actualDifficulty).ToUpper() + " " +
             cheader.difficultyLevel[LiveSetting.actualDifficulty];
         songName_Text.text = mheader.title;
-        acc_Text.text = LiveSetting.autoPlayEnabled ? "AUTOPLAY" : string.Format("{0:P2}", playResult.Acc);
+        acc_Text.text = LiveSetting.autoPlayEnabled ? "AUTOPLAY" : string.Format("{0:P2}", Mathf.FloorToInt((float)playResult.Acc * 10000) / 10000f);
         difficultCard.sprite = Resources.Load<Sprite>("UI/DifficultyCards/" + Enum.GetName(typeof(Difficulty), LiveSetting.actualDifficulty));
     }
 
