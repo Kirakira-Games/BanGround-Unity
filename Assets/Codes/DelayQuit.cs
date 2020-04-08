1737cd68a5319d40031ad422933d0c118d475dbf
LingFeng-bbben <bengy@126.com>
2020-04-08 22:55:37 +0800

    退群

---
+++ Assets/Codes/DelayQuit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayQuit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator DelayQ()
+    {
+        yield return new WaitForSeconds(6f);
+        Application.Quit();
+    }
+}
