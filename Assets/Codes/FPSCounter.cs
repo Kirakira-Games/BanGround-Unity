1c0f65478094d901810750711cdfaa6403a26a9c
KCFindstr <changyuz@usc.edu>
2020-04-09 20:27:16 -0700

    Add audio sync display in game

--- Assets/Codes/FPSCounter.cs
+++ Assets/Codes/FPSCounter.cs
@@ -11,11 +11,26 @@ public class FPSCounter : MonoBehaviour
     float lastClearTime = -1;
     int lastFPS = 0;
 
+    // Sync display
+    int audioSyncDiff;
+    Queue<int> syncQueue;
+    int totDiff;
+    private const int audioSyncCount = 30;
+
     float lostFocusTime = 0;
+
+    void ClearSync()
+    {
+        syncQueue.Clear();
+        totDiff = 0;
+        audioSyncDiff = int.MinValue;
+    }
 
     void Awake()
     {
+        syncQueue = new Queue<int>();
         text = GetComponent<Text>();
+        ClearSync();
     }
     void Update()
     {
@@ -28,6 +43,31 @@ public class FPSCounter : MonoBehaviour
 
         if (Time.timeScale == 0) return;
         frameInSec++;
-        text.text = $"FPS : {lastFPS}";//\n{Mathf.Round(Time.deltaTime * 1000)}  ms";
+
+
+        string str = $"FPS : {lastFPS}";
+        // Audio sync test
+        int? audioTime = (int?)AudioManager.Instance?.gameBGM?.GetPlaybackTime();
+        int? syncTime = AudioTimelineSync.instance?.GetTimeInMs();
+        if (audioTime.HasValue && syncTime.HasValue)
+        {
+            int diff = syncTime.Value - audioTime.Value;
+            syncQueue.Enqueue(diff);
+            totDiff += diff;
+            while (syncQueue.Count > audioSyncCount)
+            {
+                totDiff -= syncQueue.Dequeue();
+            }
+            audioSyncDiff = totDiff / syncQueue.Count;
+        }
+        else
+        {
+            ClearSync();
+        }
+        if (audioSyncDiff != int.MinValue)
+        {
+            str += $"\nSync: {(audioSyncDiff >= 0 ? "+" + audioSyncDiff : audioSyncDiff.ToString())}ms";
+        }
+        text.text = str;
     }
 }
