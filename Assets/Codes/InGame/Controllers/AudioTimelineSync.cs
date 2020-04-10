c3069ad5134a45fadb1f954865a1649acf93ddd5
KCFindstr <changyuz@usc.edu>
2020-04-10 00:28:42 -0700

    Add audio sync algorithm.

--- Assets/Codes/InGame/Controllers/AudioTimelineSync.cs
+++ Assets/Codes/InGame/Controllers/AudioTimelineSync.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Diagnostics;
 using System;
+using System.Collections.Generic;
 
 public class AudioTimelineSync : MonoBehaviour
 {
@@ -8,6 +9,14 @@ public class AudioTimelineSync : MonoBehaviour
 
     private Stopwatch watch;
     private float deltaTime;
+
+    // Sync display
+    public float smoothAudioDiff => syncQueue.Count == 0 ? float.NaN : totDiff / syncQueue.Count;
+    private Queue<float> syncQueue;
+    private const int QUEUE_SIZE = 10;
+    private float totDiff;
+    private uint prevAudioTime;
+    private static float adjustLimit;
 
     public static float BGMTimeToRealtime(float t)
     {
@@ -32,15 +41,26 @@ public class AudioTimelineSync : MonoBehaviour
         throw new NotImplementedException();
     }
 
+    private void ClearSync()
+    {
+        syncQueue.Clear();
+        totDiff = 0;
+        prevAudioTime = int.MaxValue;
+    }
+
     private void Awake()
     {
         instance = this;
         watch = new Stopwatch();
+        syncQueue = new Queue<float>();
+        ClearSync();
         deltaTime = -1e3f;
+        adjustLimit = Time.deltaTime * 0.2f * LiveSetting.SpeedCompensationSum;
     }
 
     public void Play()
     {
+        ClearSync();
         watch.Start();
     }
 
@@ -62,5 +82,29 @@ public class AudioTimelineSync : MonoBehaviour
     public void Seek(float targetTime)
     {
         deltaTime = (float)(targetTime / LiveSetting.SpeedCompensationSum - watch.Elapsed.TotalSeconds);
+    }
+
+    private void Update()
+    {
+        var bgm = AudioManager.Instance?.gameBGM;
+        if (bgm == null || bgm.GetStatus() != AudioProvider.PlaybackStatus.Playing)
+            return;
+        // Audio sync test
+        uint audioTime = bgm.GetPlaybackTime();
+        if (audioTime == prevAudioTime)
+            return;
+        prevAudioTime = audioTime;
+        float diff = audioTime / 1000f - GetTimeInS();
+        syncQueue.Enqueue(diff);
+        totDiff += diff;
+        while (syncQueue.Count > QUEUE_SIZE)
+        {
+            totDiff -= syncQueue.Dequeue();
+        }
+        // Try to sync
+        float adjust = diff * 0.2f;
+        if (Mathf.Abs(adjust) > adjustLimit)
+            adjust = adjustLimit * Mathf.Sign(diff);
+        deltaTime += adjust;
     }
 }
