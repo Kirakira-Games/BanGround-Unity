6dc675be4af9f6cc7fe5a3556da171ede6d5c770
LingFeng-bbben <bengy@126.com>
2020-04-12 07:15:25 +0800

    把屎拆成两块
    注意notestyle好像不能正常工作

--- Assets/Codes/Select/SelectManager.cs
+++ Assets/Codes/Select/SelectManager.cs
@@ -11,61 +11,10 @@ using AudioProvider;
 
 public class SelectManager : MonoBehaviour
 {
-    internal static bool letTheBassKick = false;
+    public static bool letTheBassKick = false; //AprFOOl
     public const float scroll_Min_Speed = 50f;
-    //private Button enter_Btn;
-    private Button setting_Open_Btn;
-    private Button setting_Close_Btn;
-    private Button mod_Open_Btn;
-    private Button mod_Close_Btn;
-    private Button sort_Button;
 
     private int lastIndex = -1;
-
-    private Toggle syncLine_Tog;
-    private Toggle offBeat_Tog;
-    private Toggle persp_Tog;
-    private Toggle mirrow_Tog;
-    private Slider ELP_Slider;
-    private Toggle FS_Tog;
-    private Toggle VSync_Tog;
-    private Toggle laneLight_Tog;
-    private Toggle shake_Tog;
-    private Toggle milisec_Tog;
-    private Toggle Video_Tog;
-    private NoteStyleToggleGroup noteToggles;
-    private SESelector seSelector;
-
-    /* Mods     */
-
-    // Auto
-    private Toggle auto_Tog;
-    // Double
-    private StepToggle speedUp_Tog;
-    // Half
-    private StepToggle speedDown_Tog;
-    //Sudden Death
-    private Toggle suddenDeath_Tog;
-    //Perfect
-    private Toggle perfect_Tog;
-
-    /* Mods End */
-
-    private InputField speed_Input;
-    private InputField judge_Input;
-    private InputField audio_Input;
-    private InputField size_Input;
-
-    private Slider judgeOffsetTransform;
-    private Slider far_Clip;
-    private Slider bg_Bright;
-    private Slider lane_Bright;
-    private Slider long_Bright;
-    private Slider seVolume_Input;
-    private Slider igseVolume_Input;
-    private Slider bgmVolume_Input;
-
-    private Dropdown language_Dropdown;
 
     RectTransform rt;
     RectTransform rt_v;
@@ -78,22 +27,25 @@ public class SelectManager : MonoBehaviour
     Text score;
     Text acc;
 
-    private Animator scene_Animator;
+    private Animator scene_Animator;//sort animation
+    private Button sort_Button;
 
     public GameObject songItemPrefab;
+
     [SerializeField] private TextAsset[] voices;
 
     public List<cHeader> chartList => DataLoader.chartList;
     List<GameObject> SelectButtons = new List<GameObject>();
 
     DifficultySelect difficultySelect;
-
     PlayRecords playRecords;
+    public static SelectManager instance;
 
     [HideInInspector] public ISoundTrack previewSound;
 
     private void Awake()
     {
+        instance = this;
         DataLoader.LoadAllKiraPackFromInbox();
         DataLoader.RefreshSongList();
         DataLoader.ReloadSongList();
@@ -109,8 +61,6 @@ public class SelectManager : MonoBehaviour
         InitComponent();
         LoadScoreRecord();
         InitSongList(false);
-        GetLiveSetting();
-
         PlayVoicesAtSceneIn();
 
         if (letTheBassKick)
@@ -149,92 +99,19 @@ public class SelectManager : MonoBehaviour
 
     private void InitComponent()
     {
+        //sort
         scene_Animator = GameObject.Find("SceneAnimator").GetComponent<Animator>();
         sort_Button = GameObject.Find("Sort_Button").GetComponent<Button>();
         sort_Button.onClick.AddListener(SwitchSort);
 
+        //Main Scroll View
         rt = GameObject.Find("SongContent").GetComponent<RectTransform>();
         rt_v = GameObject.Find("Song Scroll View").GetComponent<RectTransform>();
         rt_s = GameObject.Find("Song Scroll View").GetComponent<ScrollRect>();
         dh = GameObject.Find("Song Scroll View").GetComponent<DragHandler>();
         lg = GameObject.Find("SongContent").GetComponent<VerticalLayoutGroup>();
 
-        //enter_Btn = GameObject.Find("Enter_Btn").GetComponent<Button>();
-        setting_Open_Btn = GameObject.Find("SettingOpenBtn").GetComponent<Button>();
-        setting_Close_Btn = GameObject.Find("SettingButton_Close").GetComponent<Button>();
-        mod_Open_Btn = GameObject.Find("ModOpenBtn").GetComponent<Button>();
-        mod_Close_Btn = GameObject.Find("ModButton_Close").GetComponent<Button>();
-
-        syncLine_Tog = GameObject.Find("Sync_Toggle").GetComponent<Toggle>();
-        offBeat_Tog = GameObject.Find("Offbeat_Toggle").GetComponent<Toggle>();
-        mirrow_Tog = GameObject.Find("Mirrow_Toggle").GetComponent<Toggle>();
-        persp_Tog = GameObject.Find("Perspective_Toggle").GetComponent<Toggle>();
-        FS_Tog = GameObject.Find("Fullscreen_Toggle").GetComponent<Toggle>();
-        VSync_Tog = GameObject.Find("VSync_Toggle").GetComponent<Toggle>();
-        noteToggles = GameObject.Find("Note_Group").GetComponent<NoteStyleToggleGroup>();
-        seSelector= GameObject.Find("SEGroup").GetComponent<SESelector>();
-        Video_Tog = GameObject.Find("Video_Toggle").GetComponent<Toggle>();
-
-        speed_Input = GameObject.Find("Speed_Input").GetComponent<InputField>();
-        judge_Input = GameObject.Find("Judge_Input").GetComponent<InputField>();
-        audio_Input = GameObject.Find("Audio_Input").GetComponent<InputField>();
-        size_Input = GameObject.Find("Size_Input").GetComponent<InputField>();
-
-        judgeOffsetTransform = GameObject.Find("Judge_Offset_Transform_Slider").GetComponent<Slider>();
-        far_Clip = GameObject.Find("Far_Clip_Slider").GetComponent<Slider>();
-        bg_Bright = GameObject.Find("BG_Bri_Slider").GetComponent<Slider>();
-        lane_Bright = GameObject.Find("Lane_Bri_Slider").GetComponent<Slider>();
-        long_Bright = GameObject.Find("Long_Bri_Slider").GetComponent<Slider>();
-        seVolume_Input = GameObject.Find("SeVolume_Input").GetComponent<Slider>();
-        igseVolume_Input = GameObject.Find("IGSEVolume_Input").GetComponent<Slider>();
-        bgmVolume_Input = GameObject.Find("BGMVolume_Input").GetComponent<Slider>();
-        ELP_Slider = GameObject.Find("ELP_Slider").GetComponent<Slider>();
-
-        auto_Tog = GameObject.Find("Autoplay_Toggle").GetComponent<Toggle>();
-        speedDown_Tog = GameObject.Find("Half_Toggle").GetComponent<StepToggle>();
-        speedUp_Tog = GameObject.Find("Double_Toggle").GetComponent<StepToggle>();
-        suddenDeath_Tog = GameObject.Find("SuddenDeath_Toggle").GetComponent<Toggle>();
-        perfect_Tog = GameObject.Find("Perfect_Toggle").GetComponent<Toggle>();
-
-        milisec_Tog = GameObject.Find("Milisec_Toggle").GetComponent<Toggle>();
-        laneLight_Tog = GameObject.Find("LaneLight_Toggle").GetComponent<Toggle>();
-        shake_Tog = GameObject.Find("Shake_Toggle").GetComponent<Toggle>();
-
-        language_Dropdown = GameObject.Find("Language_Dropdown").GetComponent<Dropdown>();
-        language_Dropdown.onValueChanged.AddListener(OnLanuageChanged);
-
-        setting_Open_Btn.onClick.AddListener(OpenSetting);
-        setting_Close_Btn.onClick.AddListener(CloseSetting);
-        mod_Open_Btn.onClick.AddListener(OpenMod);
-        mod_Close_Btn.onClick.AddListener(CloseMod);
-
-        GameObject.Find("Speed>").GetComponent<Button>().onClick.AddListener(() => { speed_Input.text = (float.Parse(speed_Input.text) + 0.1f).ToString(); });
-        GameObject.Find("Speed<").GetComponent<Button>().onClick.AddListener(() => { speed_Input.text = (float.Parse(speed_Input.text) - 0.1f).ToString(); });
-        GameObject.Find("Speed>>").GetComponent<Button>().onClick.AddListener(() => { speed_Input.text = (float.Parse(speed_Input.text) + 1f).ToString(); });
-        GameObject.Find("Speed<<").GetComponent<Button>().onClick.AddListener(() => { speed_Input.text = (float.Parse(speed_Input.text) - 1f).ToString(); });
-        speed_Input.onValueChanged.AddListener((string a) =>
-        {
-            if (float.Parse(speed_Input.text) < 1) { speed_Input.text = "11"; }
-            if (float.Parse(speed_Input.text) > 11f) { speed_Input.text = "1"; }
-            speed_Input.text = string.Format("{0:F1}", float.Parse(speed_Input.text));
-        });
-
-        GameObject.Find("Size>").GetComponent<Button>().onClick.AddListener(() => { size_Input.text = (float.Parse(size_Input.text) + 0.1f).ToString(); });
-        GameObject.Find("Size<").GetComponent<Button>().onClick.AddListener(() => { size_Input.text = (float.Parse(size_Input.text) - 0.1f).ToString(); });
-        size_Input.onValueChanged.AddListener((string a) =>
-        {
-            if (float.Parse(size_Input.text) < 0.1f) { size_Input.text = "3"; }
-            if (float.Parse(size_Input.text) > 2f) { size_Input.text = "0.1"; }
-            size_Input.text = string.Format("{0:F1}", float.Parse(size_Input.text));
-        });
-
-        GameObject.Find("JudOff>").GetComponent<Button>().onClick.AddListener(() => { judge_Input.text = (float.Parse(judge_Input.text) + 1f).ToString(); });
-        GameObject.Find("JudOff<").GetComponent<Button>().onClick.AddListener(() => { judge_Input.text = (float.Parse(judge_Input.text) - 1f).ToString(); });
-
-        GameObject.Find("AudOff>").GetComponent<Button>().onClick.AddListener(() => { audio_Input.text = (float.Parse(audio_Input.text) + 1f).ToString(); });
-        GameObject.Find("AudOff<").GetComponent<Button>().onClick.AddListener(() => { audio_Input.text = (float.Parse(audio_Input.text) - 1f).ToString(); });
-        //live setting init
-
+        //Marks
         Rank = GameObject.Find("Rank").GetComponent<RawImage>();
         clearMark = GameObject.Find("ClearMark").GetComponent<RawImage>();
         score = GameObject.Find("ScoreHistory").GetComponent<Text>();
@@ -242,17 +119,11 @@ public class SelectManager : MonoBehaviour
 
         difficultySelect = GameObject.Find("DifficultySelect").GetComponent<DifficultySelect>();
 
+        //Sort
         GameObject.Find("ButtonName").GetComponent<Button>().onClick.AddListener(() => { LiveSetting.sort = Sorter.SongName; SwitchSort(); InitSongList(); });
         GameObject.Find("ButtonArtist").GetComponent<Button>().onClick.AddListener(() => { LiveSetting.sort = Sorter.SongArtist; SwitchSort(); InitSongList(); });
         GameObject.Find("ButtonCharter").GetComponent<Button>().onClick.AddListener(() => { LiveSetting.sort = Sorter.ChartAuthor; SwitchSort(); InitSongList(); });
         GameObject.Find("ButtonLevel").GetComponent<Button>().onClick.AddListener(() => { LiveSetting.sort = Sorter.ChartDif; SwitchSort(); InitSongList(); });
-
-#if !(UNITY_STANDALONE || UNITY_WSA)
-        GameObject.Find("Fullscreen").SetActive(false);
-        GameObject.Find("Fullscreen_Toggle").SetActive(false);
-        GameObject.Find("VSync").SetActive(false);
-        GameObject.Find("VSync_Toggle").SetActive(false);
-#endif
     }
     void LoadScoreRecord()
     {
@@ -515,8 +386,6 @@ public class SelectManager : MonoBehaviour
     bool isFirstPlay = true;
     IEnumerator PlayPreview()
     {
-        
-
         mHeader mheader = DataLoader.GetMusicHeader(LiveSetting.CurrentHeader.mid);
 
         if (previewSound != null) {
@@ -551,165 +420,10 @@ public class SelectManager : MonoBehaviour
         }
     }
 
-    //Setting And Mod------------------------------
-    void OpenSetting()
-    {
-        //GetModStatus();
-        GameObject.Find("Setting_Canvas").GetComponent<Animator>().Play("DropDown");
-        if (GameObject.Find("Sound_Panel") != null)
-        {
-            previewSound?.Pause();
-        }
-    }
-    void CloseSetting()
-    {
-        previewSound.Play();
-        GameObject.Find("Setting_Canvas").GetComponent<Animator>().Play("FlyUp");
-        SetLiveSetting();
-        File.WriteAllText(LiveSetting.settingsPath, JsonConvert.SerializeObject(new LiveSettingTemplate()));
-
-        AudioManager.Provider.SetSoundEffectVolume(LiveSetting.seVolume, SEType.Common);
-        AudioManager.Provider.SetSoundEffectVolume(LiveSetting.igseVolume, SEType.InGame);
-        AudioManager.Provider.SetSoundTrackVolume(LiveSetting.bgmVolume);
-    }
-    void OpenMod()
-    {
-        GameObject.Find("Setting_Canvas").GetComponent<Animator>().Play("ModDropDown");
-    }
-    void CloseMod()
-    {
-        GameObject.Find("Setting_Canvas").GetComponent<Animator>().Play("ModFlyUp");
-        SetLiveSetting();
-    }
-    void GetLiveSetting()
-    {
-        if (letTheBassKick)
-        {
-            LiveSetting.farClip = 169f;
-        }
-        speed_Input.text = LiveSetting.noteSpeed.ToString();
-        judge_Input.text = LiveSetting.judgeOffset.ToString();
-        audio_Input.text = LiveSetting.audioOffset.ToString();
-        size_Input.text = LiveSetting.noteSize.ToString();
-        syncLine_Tog.isOn = LiveSetting.syncLineEnabled;
-        offBeat_Tog.isOn = LiveSetting.grayNoteEnabled;
-        mirrow_Tog.isOn = LiveSetting.mirrowEnabled;
-        persp_Tog.isOn = LiveSetting.bangPerspective;
-        ELP_Slider.value = LiveSetting.ELPValue;
-        FS_Tog.isOn = Screen.fullScreen;
-        VSync_Tog.isOn = QualitySettings.vSyncCount == 1;
-        laneLight_Tog.isOn = LiveSetting.laneLight;
-        shake_Tog.isOn = LiveSetting.shakeFlick;
-        milisec_Tog.isOn = LiveSetting.dispMilisec;
-        Video_Tog.isOn = LiveSetting.useVideo;
-
-        judgeOffsetTransform.value = LiveSetting.offsetTransform;
-        far_Clip.value = LiveSetting.farClip;
-        bg_Bright.value = LiveSetting.bgBrightness;
-        lane_Bright.value = LiveSetting.laneBrightness;
-        long_Bright.value = LiveSetting.longBrightness;
-
-        seVolume_Input.value = LiveSetting.seVolume;
-        igseVolume_Input.value = LiveSetting.igseVolume;
-        bgmVolume_Input.value = LiveSetting.bgmVolume;
-
-        noteToggles.SetStyle(LiveSetting.noteStyle);
-        seSelector.SetSE(LiveSetting.seStyle);
-        language_Dropdown.value = (int)LiveSetting.language;
-
-        GetModStatus();
-    }
-    void GetModStatus()
-    {
-        auto_Tog.isOn = LiveSetting.autoPlayEnabled;
-
-        speedDown_Tog.SetStep(LiveSetting.attachedMods);
-        speedUp_Tog.SetStep(LiveSetting.attachedMods);
-        suddenDeath_Tog.isOn = LiveSetting.attachedMods.Contains(SuddenDeathMod.Instance);
-        perfect_Tog.isOn = LiveSetting.attachedMods.Contains(PerfectMod.Instance);
-    }
-
-    public void OnLanuageChanged(int value)
-    {
-        LiveSetting.language = (Language)value;
-        LocalizedStrings.Instanse.ReloadLanguageFile(LiveSetting.language);
-        LocalizedText.ReloadAll();
-    }
-
-    void SetLiveSetting()
-    {
-        try
-        {
-            LiveSetting.noteSpeed = float.Parse(speed_Input.text);
-            LiveSetting.judgeOffset = int.Parse(string.IsNullOrWhiteSpace(judge_Input.text) ? 0.ToString() : judge_Input.text);
-            LiveSetting.audioOffset = int.Parse(string.IsNullOrWhiteSpace(audio_Input.text) ? 0.ToString() : audio_Input.text);
-            LiveSetting.noteSize = float.Parse(size_Input.text);
-            LiveSetting.seVolume = seVolume_Input.value;
-            LiveSetting.igseVolume = igseVolume_Input.value;
-            LiveSetting.bgmVolume = bgmVolume_Input.value;
-            LiveSetting.syncLineEnabled = syncLine_Tog.isOn;
-            LiveSetting.grayNoteEnabled = offBeat_Tog.isOn;
-            LiveSetting.mirrowEnabled = mirrow_Tog.isOn;
-            LiveSetting.autoPlayEnabled = auto_Tog.isOn;
-            LiveSetting.bangPerspective = persp_Tog.isOn;
-            LiveSetting.ELPValue = ELP_Slider.value;
-            LiveSetting.laneLight = laneLight_Tog.isOn;
-            LiveSetting.shakeFlick = shake_Tog.isOn;
-            LiveSetting.dispMilisec = milisec_Tog.isOn;
-            LiveSetting.useVideo = Video_Tog.isOn;
-#if (UNITY_STANDALONE || UNITY_WSA)
-            if (FS_Tog.isOn)
-            {
-                var r = Screen.resolutions[Screen.resolutions.Length - 1];
-                Screen.SetResolution(r.width, r.height, FullScreenMode.FullScreenWindow);
-                Screen.fullScreen = true;
-            }
-            else
-            {
-                var r = Screen.resolutions[Screen.resolutions.Length - 2];
-                Screen.SetResolution(r.width, r.height, FullScreenMode.Windowed);
-                Screen.fullScreen = false;
-            }
-
-            if(VSync_Tog.isOn)
-            {
-                QualitySettings.vSyncCount = 1;
-            }
-            else
-            {
-                QualitySettings.vSyncCount = 0;
-            }
-#endif
-            LiveSetting.offsetTransform = judgeOffsetTransform.value;
-            LiveSetting.farClip = far_Clip.value;
-            LiveSetting.bgBrightness = bg_Bright.value;
-            LiveSetting.laneBrightness = lane_Bright.value;
-            LiveSetting.longBrightness = long_Bright.value;
-
-            LiveSetting.noteStyle = noteToggles.GetStyle();
-            LiveSetting.seStyle = seSelector.GetSE();
-
-            LiveSetting.RemoveAllMods();
-            LiveSetting.attachedMods.Clear();
-            LiveSetting.AddMod(speedUp_Tog.GetStep());
-            LiveSetting.AddMod(speedDown_Tog.GetStep());
-
-            if (suddenDeath_Tog.isOn) LiveSetting.AddMod(SuddenDeathMod.Instance);
-
-            if (perfect_Tog.isOn) LiveSetting.AddMod(PerfectMod.Instance);
-        }
-        catch(Exception e)
-        {
-            Console.WriteLine(e.Message);
-            throw e;
-        }
-    }
-
-    //============================================
     public void OnEnterPressed()
     {
         StartCoroutine(PreviewFadeOut());
-        SetLiveSetting();
+        SettingAndMod.instance.SetLiveSetting();
         File.WriteAllText(LiveSetting.settingsPath, JsonConvert.SerializeObject(new LiveSettingTemplate()));
         PlayVoicesAtSceneOut();
         SceneLoader.LoadScene("Select", "InGame", true);
