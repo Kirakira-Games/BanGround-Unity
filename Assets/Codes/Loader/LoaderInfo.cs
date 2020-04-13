164c2c3f0b47e693e129096e50fe05d9bdbc289b
Frenqy <411937015@qq.com>
2020-04-13 23:56:33 +0800

    歌曲信息小剧场-修修补补

--- Assets/Codes/Loader/LoaderInfo.cs
+++ Assets/Codes/Loader/LoaderInfo.cs
@@ -21,28 +21,46 @@ public class LoaderInfo : MonoBehaviour
     [SerializeField] private Text songNoteCount;
 
     const string NameFormat = "{0}";// Offset Rhythm Test
-    const string BPMFormat = "BPM {0}-{1}";// BPM 13-269
+    //const string BPMFormat = "BPM {0}-{1}";// BPM 13-269
     const string LevelAndCharterFormat = "{0} {1} BY {2}"; // EASY 26 BY User
     const string ArtistFormat = "{0}";// Produced by KIRAKIRA sound team "bbben"<啥比
     const string NoteCountFormat = "Note {0}";//Note 999
 
-    void Start()
+    private void Start()
     {
-        //Get Info
+        GetInfo();
+        ShowInfo();
+    }
+
+    private void GetInfo()
+    {
         chartHeader = LiveSetting.CurrentHeader;
         musicHeader = DataLoader.GetMusicHeader(LiveSetting.CurrentHeader.mid);
         chart = DataLoader.LoadChart(LiveSetting.CurrentHeader.sid, (Difficulty)LiveSetting.actualDifficulty);
         chartData = ChartLoader.LoadChart(chart);
+    }
 
+    private void ShowInfo()
+    {
         //Show Info
-        var tex = KiraFilesystem.Instance.ReadTexture2D(DataLoader.GetBackgroundPath(LiveSetting.CurrentHeader.sid).Item1);
-        songImg.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        var path = DataLoader.GetBackgroundPath(LiveSetting.CurrentHeader.sid).Item1;
+        if (path != null && KiraFilesystem.Instance.Exists(path))
+        {
+            var tex = KiraFilesystem.Instance.ReadTexture2D(path);
+            songImg.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        }
 
         songName.text = string.Format(NameFormat, musicHeader.title);
-        songBPM.text = string.Format(BPMFormat, chartData.bpm.Min(o => o.value), chartData.bpm.Max(o => o.value));
+        songBPM.text = GetBPM();
         songLevelAndCharter.text = string.Format(LevelAndCharterFormat, Enum.GetName(typeof(Difficulty), chart.Difficulty).ToUpper(), chart.level, chartHeader.authorNick);
         songArtist.text = string.Format(ArtistFormat, musicHeader.artist);
         songNoteCount.text = string.Format(NoteCountFormat, chartData.numNotes);
     }
 
+    private string GetBPM()
+    {
+        var min = chartData.bpm.Min(o => o.value);
+        var max = chartData.bpm.Max(o => o.value);
+        return min == max ? $"BPM {min}" : $"BPM {min}-{max}";
+    }
 }
