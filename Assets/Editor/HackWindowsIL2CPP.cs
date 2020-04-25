a830c59f02ddc2885e9d52b09371a44ab1f25225
Frenqy <411937015@qq.com>
2020-04-25 23:09:59 +0800

    抄的作业没改好

--- Assets/Editor/HackWindowsIL2CPP.cs
+++ Assets/Editor/HackWindowsIL2CPP.cs
@@ -6,9 +6,9 @@ public class HackWindowsIL2CPP
     public static void PreExport()
     {
         string status = "Default";
- 
-#if UNITY_CLOUD_BUILD
-        PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.Mono2x);
+
+#if UNITY_CLOUD_BUILD && UNITY_STANDALONE_WIN
+        UnityEditor.PlayerSettings.SetScriptingBackend(UnityEditor.BuildTargetGroup.Standalone, UnityEditor.ScriptingImplementation.Mono2x);
         status = "Customized To Use Mono";
 #endif
         Debug.Log($"Build Configuration - {status}");
