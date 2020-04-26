ddc9309cfc7538d5e665b3a2ddbb988e3982bee3
LingFeng-bbben <bengy@126.com>
2020-04-27 00:35:35 +0800

    看背景老婆

--- Assets/Codes/FixOrthographicBackground.cs
+++ Assets/Codes/FixOrthographicBackground.cs
@@ -4,9 +4,15 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class FixOrthographicBackground : FixBackground
 {
+    Image fullback;
+    private void Awake()
+    {
+        fullback = GameObject.Find("FullBaCkGrouNd").GetComponent<Image>();
+    }
     protected override void Start()
     {
         mainCam = Camera.main;
@@ -25,5 +31,13 @@ public class FixOrthographicBackground : FixBackground
         float scale = Mathf.Max(camSize.x / spriteSize.x, camSize.y / spriteSize.y);
         //transform.localScale = new Vector3(camSize.x / spriteSize.x, camSize.y / spriteSize.y, 1);
         transform.localScale = Vector3.one * scale;
+    }
+
+    protected override void GetAndSetBG(string path)
+    {
+        var tex = KiraFilesystem.Instance.ReadTexture2D(path);
+        render.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        fullback.sprite = render.sprite;
+        UpdateScale();
     }
 }
---
+++ Assets/Codes/Select/AdvanceUIComponent/ViewFullBackground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ViewFullBackground : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    List<GameObject> DisableGmobjs = new List<GameObject>();
+    List<GameObject> EnableGmobjs = new List<GameObject>();
+    private void Start()
+    {
+        DisableGmobjs.Add(GameObject.Find("Setting_Canvas"));
+        DisableGmobjs.Add(GameObject.Find("Selet_Canvas"));
+        EnableGmobjs.Add(GameObject.Find("FullBaCkGrouNd"));
+
+        foreach (GameObject o in EnableGmobjs) o.SetActive(false);
+    }
+
+    public void OnPointerDown(PointerEventData a)
+    {
+        foreach (GameObject o in DisableGmobjs) o.SetActive(false);
+        foreach (GameObject o in EnableGmobjs) o.SetActive(true);
+    }
+
+    public void OnPointerUp(PointerEventData a)
+    {
+        foreach (GameObject o in DisableGmobjs) o.SetActive(true);
+        foreach (GameObject o in EnableGmobjs) o.SetActive(false);
+    }
+}
