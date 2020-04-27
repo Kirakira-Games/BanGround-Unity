6fc961ec3194aa2ffbeb3d3c568284f5ea37c3ea
LingFeng-bbben <bengy@126.com>
2020-04-27 23:09:22 +0800

    鼠标滚轮优化

---
+++ Assets/Codes/Select/AdvanceUIComponent/MouseWheelHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseWheelHandler : MonoBehaviour,IScrollHandler
+{
+    public void OnScroll(PointerEventData p)
+    {
+        if (p.scrollDelta.y > 0)
+        {
+            if (LiveSetting.currentChart < DataLoader.chartList.Count-1)
+                SelectManager.instance.SelectSong(LiveSetting.currentChart + 1);
+        }
+        else
+            SelectManager.instance.SelectSong(LiveSetting.currentChart - 1);
+    }
+}
