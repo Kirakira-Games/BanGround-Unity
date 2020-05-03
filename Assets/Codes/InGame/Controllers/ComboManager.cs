6442bd1b4961398e8c8f97cafb8b5ee42eba58d5
Frenqy <411937015@qq.com>
2020-05-03 11:19:19 +0800

    完善AP/FC指示器；input动画和按钮

--- Assets/Codes/InGame/Controllers/ComboManager.cs
+++ Assets/Codes/InGame/Controllers/ComboManager.cs
@@ -19,11 +19,13 @@ public class ComboManager : MonoBehaviour
 
     private int[] combo;
 
+    [SerializeField] private Material[] comboMat;
     private Text comboText;
     private Animator comboAnimator;
     private GradeColorChange scoreDisplay;
     private Image[] comboImg;
     private Sprite[] comboSprite;
+    private ClearFlag flag = ClearFlag.AP;
 
     private void Awake()
     {
@@ -49,6 +51,9 @@ public class ComboManager : MonoBehaviour
 
     public void UpdateCombo(JudgeResult result)
     {
+        if (flag == ClearFlag.AP && result > JudgeResult.Perfect) { flag = ClearFlag.FC; UpdateComboMat(); }
+        if (flag == ClearFlag.FC && result > JudgeResult.Great) { flag = ClearFlag.None; UpdateComboMat(); }
+
         int intResult = (int)result;
         judgeCount[intResult]++;
         acc += accRate[intResult];
@@ -99,6 +104,14 @@ public class ComboManager : MonoBehaviour
         }
     }
 
+    private void UpdateComboMat()
+    {
+        for (int i = 0; i < comboImg.Length; i++)
+        {
+            comboImg[i].material = comboMat[(int)flag];
+        }
+    }
+
     private static double Accumulate(int segSize, double segDelta, int num)
     {
         int segNum = num / segSize;
@@ -127,4 +140,7 @@ public class ComboManager : MonoBehaviour
             maxScore = numNotes + Accumulate(50, 0.005, numNotes) + Accumulate(100, 0.005, numNotes);
         }
     }
+
+    [Flags]
+    enum ClearFlag { None,FC,AP}
 }
