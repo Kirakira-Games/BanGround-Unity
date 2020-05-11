ac87440bf341d9c612e65bd8145581d09f463a54
GEEKiDoS <geek_ds@foxmail.com>
2020-05-11 09:23:15 +0800

    背景图黑边修复

--- Assets/Codes/Filesystem/KiraFilesystem.cs
+++ Assets/Codes/Filesystem/KiraFilesystem.cs
@@ -300,6 +300,7 @@ namespace System.IO
         {
             Texture2D tex = new Texture2D(2, 2);
             tex.LoadImage(Read(fileName));
+            tex.wrapMode = TextureWrapMode.Mirror;
 
             return tex;
         }
