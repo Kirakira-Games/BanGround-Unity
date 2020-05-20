302a06fbc87cd420051b6177bd59f08d58143da7
KCFindstr <changyuz@usc.edu>
2020-05-19 18:42:54 -0700

    在做了在做了

---
+++ Assets/Codes/Mapping/Core/CoreMonoBehavior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace BGEditor
+{
+    public class CoreMonoBehavior : MonoBehaviour
+    {
+        private ChartCore mCore;
+        public ChartCore Core => mCore == null ? mCore = ChartCore.Instance : mCore;
+    }
+}
