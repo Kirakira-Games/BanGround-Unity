01be460592db2280dce6975c512b04c612cad764
Frenqy <411937015@qq.com>
2020-05-26 01:04:07 +0800

    Get user nickname and avatar

---
+++ Assets/Codes/Title/UserInfo.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+using System.Text;
+
+public class UserInfo : MonoBehaviour
+{
+    public static UserInfo Instance { get; private set; }
+
+    private const string Prefix = "https://api.reikohaku.fun/api";
+    private const string API = "/auth/info?username=";
+    private const string FullAPI = Prefix + API;
+
+    public UserInfoRespone result;
+    public string username;
+
+    private Text username_Text;
+    private Image userAvatar;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        username_Text = GameObject.Find("Username").GetComponent<Text>();
+        userAvatar = GameObject.Find("Avatar").GetComponent<Image>();
+    }
+
+    public void ShowUserInfo()
+    {
+        StartCoroutine(GetUserInfo());
+    }
+
+    private IEnumerator GetUserInfo()
+    {
+        var req = new KirakiraWebRequest<UserInfoRespone>();
+        yield return req.Get(FullAPI + username);
+        result = req.resp;
+        username_Text.text = result.nickname;
+        using (UnityWebRequest ub = UnityWebRequestTexture.GetTexture(result.avatar))
+        {
+            yield return ub.SendWebRequest();
+            var tex = DownloadHandlerTexture.GetContent(ub);
+            userAvatar.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        }
+    }
+}
+
+public class UserInfoRespone
+{
+    public bool status;
+    public string nickname;
+    public string avatar;
+}
