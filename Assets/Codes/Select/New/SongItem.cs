585e8f0eb8e13c1d12b194423b2908aa6660d075
Frenqy <411937015@qq.com>
2020-05-27 09:47:23 +0800

    2%:预制体与代码测试

---
+++ Assets/Codes/Select/New/SongItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SongItem : MonoBehaviour
+{
+    public Text songName;
+    public Text songArtist;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void ScrollCellIndex(int idx)
+    {
+        songName.text = idx.ToString();
+        gameObject.name = idx.ToString();
+        Debug.Log(idx);
+    }
+}
