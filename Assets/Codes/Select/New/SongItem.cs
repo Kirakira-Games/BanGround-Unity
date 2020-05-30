d04c16c8279796045600c35315af734f2ef0f6ae
GEEKiDoS <geek_ds@foxmail.com>
2020-05-30 23:13:18 +0800

    换个思路hack

--- Assets/Codes/Select/New/SongItem.cs
+++ Assets/Codes/Select/New/SongItem.cs
@@ -11,12 +11,8 @@ public class SongItem : MonoBehaviour
     public Text songName;
     public Text songArtist;
 
-    public RectTransform rectTransform = null;
-    public bool isDummy = false;
-
     void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
     }
 
     void Update()
@@ -26,23 +22,10 @@ public class SongItem : MonoBehaviour
 
     void ScrollCellIndex(int idx)
     {
-        if(idx == 0)
-        {
-            var trans = new Color(0, 0, 0, 0);
+        cHeader = DataLoader.chartList[idx];
+        mHeader = DataLoader.GetMusicHeader(cHeader.mid);
 
-            songName.color = trans;
-            songArtist.color = trans;
-            GetComponent<Image>().color = trans;
-
-            isDummy = true;
-        }
-        else
-        {
-            cHeader = DataLoader.chartList[idx - 1];
-            mHeader = DataLoader.GetMusicHeader(cHeader.mid);
-
-            songName.text = mHeader.title;
-            songArtist.text = mHeader.artist;
-        }
+        songName.text = mHeader.title;
+        songArtist.text = mHeader.artist;
     }
 }
