e07319ab967115ce07160dc20f6d41d5e32aa3e5
GEEKiDoS <geek_ds@foxmail.com>
2020-05-30 22:32:52 +0800

    hack不动了

--- Assets/Codes/Select/New/SelectManager.cs
+++ Assets/Codes/Select/New/SelectManager.cs
@@ -15,6 +15,12 @@ public class SelectManager : MonoBehaviour
     public Text m_txtArtist;
     public Text m_txtCharter;
 
+    private float m_flYMid;
+
+    private SongItem m_siDummy;
+
+    private Vector2 test;
+
     private void Awake()
     {
          
@@ -26,33 +32,53 @@ public class SelectManager : MonoBehaviour
         DataLoader.RefreshSongList();
         DataLoader.ReloadSongList();
 
-        m_srSongList.totalCount = DataLoader.chartList.Count;
+        m_srSongList.totalCount = DataLoader.chartList.Count + 1;
         m_srSongList.RefillCells();
+
+        Vector3[] vector3s = new Vector3[4];
+        m_srSongList.gameObject.GetComponent<RectTransform>().GetWorldCorners(vector3s);
+        m_flYMid = (vector3s[0].y + vector3s[1].y + vector3s[2].y + vector3s[3].y) * 0.5f;
+
+        test = new Vector2(0, m_srSongList.gameObject.GetComponent<RectTransform>().rect.height / 4 - m_tfContent.rect.height);
+
+        foreach (RectTransform child in m_tfContent)
+        {
+            var si = child.GetComponent<SongItem>();
+            if (si.isDummy)
+            {
+                m_siDummy = si;
+                break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(m_siDummy.rectTransform != null)
+        {
+            m_siDummy.rectTransform.sizeDelta = test;
+        }
     }
 
     private void LateUpdate()
     {
         float minDist = 10240;
-        float minDistDelta = 10240;
 
         SongItem targetSong = null;
 
-        float Ymid = m_tfContent.rect.height / 12;
+        Vector3[] childVector3s = new Vector3[4];
 
-        foreach(RectTransform child in m_tfContent)
+        foreach (RectTransform child in m_tfContent)
         {
-            float dist = Math.Abs(Ymid - child.rect.y);
-            float delta = minDist - dist;
-            if (delta < minDistDelta)
-            {
-                minDistDelta = delta;
-                minDist = dist;
+            child.GetWorldCorners(childVector3s);
 
-                targetSong = child.gameObject.GetComponent<SongItem>();
-            }
-            else
+            var yPos = (childVector3s[0].y + childVector3s[1].y + childVector3s[2].y + childVector3s[3].y) * 0.5f;
+
+            float dist = Mathf.Abs(m_flYMid - yPos);
+            if (dist < minDist)
             {
-                break;
+                minDist = dist;
+                targetSong = child.gameObject.GetComponent<SongItem>();
             }
         }
 
