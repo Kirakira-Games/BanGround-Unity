471fe6b33bc89b369191e3bc8b98309aa187ae0a
Frenqy <411937015@qq.com>
2020-06-16 15:43:34 +0800

    进度条

---
+++ Assets/Codes/InGame/Controllers/TimeProgressManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AudioProvider;
+
+public class TimeProgressManager : MonoBehaviour
+{
+    private Slider TimeProgress;
+    private uint audioLength;
+    private ISoundTrack gameBGM;
+
+    void Start()
+    {
+        TimeProgress = GetComponent<Slider>();
+        gameBGM = AudioManager.Instance.gameBGM;
+    }
+
+    void Update()
+    {
+        if (AudioManager.Instance.isInGame)
+        {
+            TimeProgress.value = gameBGM.GetPlaybackTime() / (float)gameBGM.GetLength();
+        }
+    }
+}
