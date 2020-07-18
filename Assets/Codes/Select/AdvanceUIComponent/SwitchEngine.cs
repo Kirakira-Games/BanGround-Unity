181d90b266a68de6f439f55419a2853f4fd670c3
Frenqy <411937015@qq.com>
2020-07-19 07:38:41 +0800

    GEEKiDoS

--- Assets/Codes/Select/AdvanceUIComponent/SwitchEngine.cs
+++ Assets/Codes/Select/AdvanceUIComponent/SwitchEngine.cs
@@ -25,9 +25,14 @@ public class SwitchEngine : MonoBehaviour
             if (on) snd_engine.Set("Fmod");
         });
 
-        bool fmod = snd_engine == "Fmod";
-        engineTgs[0].isOn = !fmod;
-        engineTgs[1].isOn = fmod;
+        engineTgs[2].onValueChanged.AddListener(on =>
+        {
+            if (on) snd_engine.Set("Unity");
+        });
+
+        engineTgs[0].isOn = snd_engine == "Bass";
+        engineTgs[1].isOn = snd_engine == "Fmod";
+        engineTgs[2].isOn = snd_engine == "Unity";
 
     }
 }
