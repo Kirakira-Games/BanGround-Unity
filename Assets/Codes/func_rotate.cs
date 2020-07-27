c57567eaf66a60f86df7505d67d808942114bd14
GEEKiDoS <geek_ds@foxmail.com>
2020-07-27 13:49:12 +0800

    转起来了 转起来了（狂喜）

---
+++ Assets/Codes/func_rotate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Codes
+{
+    class func_rotate : MonoBehaviour
+    {
+        public float speed;
+
+        private Vector3 angles;
+
+        private void Start()
+        {
+            angles = transform.rotation.eulerAngles;
+        }
+
+        private void Update()
+        {
+            angles.z += speed;
+            transform.rotation = Quaternion.Euler(angles.x, angles.y, angles.z);
+        }
+    }
+}
