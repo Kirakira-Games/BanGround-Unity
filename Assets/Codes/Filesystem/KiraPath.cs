810aea450b8272dad9c264c9b8e750abacf867a5
KCFindstr <changyuz@usc.edu>
2020-09-25 00:41:43 -0700

    Fix kirafs, add KiraPath to replace System.IO.Path

---
+++ Assets/Codes/Filesystem/KiraPath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace BanGround
+{
+    public static class KiraPath
+    {
+        public static string Combine(params string[] paths)
+        {
+            return Path.Combine(paths).Replace('\\', '/');
+        }
+
+        public static string GetDirectoryName(string path)
+        {
+            return Path.GetDirectoryName(path).Replace('\\', '/');
+        }
+    }
+}
