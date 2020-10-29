993d37782f876403e95c78ae94c3024ab7e28e26
KCFindstr <changyuz@usc.edu>
2020-10-28 21:18:18 -0700

    Remove preload scene. Add chart selection screen in community view. (1/3)

--- Assets/Codes/Community/StoreController.cs
+++ Assets/Codes/Community/StoreController.cs
@@ -1,14 +1,85 @@
-﻿using BanGround.Community;
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using WebSocketSharp;
 using Zenject;
 
-namespace Assets.Codes.Community
+namespace BanGround.Community
 {
-    public class StoreController : MonoBehaviour
+    public enum StoreViewType
+    {
+        Song,
+        Chart
+    }
+
+    class StoreViewState
+    {
+        public StoreViewType Type;
+        public List<StoreItem> Items = new List<StoreItem>();
+        public float NormalizedY = 0;
+        public int Offset = 0;
+        public bool HasMorePages = true;
+        public string SearchText = "";
+        public string Title = "";
+        public SongItem Song;
+    }
+
+    class StoreStack
+    {
+        private StoreController mController;
+        private readonly Stack<StoreViewState> mStack = new Stack<StoreViewState>();
+
+        public StoreStack(StoreController controller)
+        {
+            mController = controller;
+        }
+
+        public int Count => mStack.Count;
+        public StoreViewState Peek() => Count > 0 ? mStack.Peek() : null;
+
+        public void RefreshState()
+        {
+            if (Count == 0)
+                return;
+            var state = Peek();
+            mController.StoreRect.normalizedPosition = new Vector2(mController.StoreRect.normalizedPosition.x, state.NormalizedY);
+            mController.SearchBar.text = state.SearchText;
+            mController.TitleText.text = state.Title;
+        }
+
+        public StoreViewState Pop()
+        {
+            var ret = mStack.Pop();
+            ret.Items.ForEach(item => GameObject.Destroy(item.gameObject));
+            if (mStack.Count > 0)
+            {
+                Peek().Items.ForEach(item => item.gameObject.SetActive(true));
+            }
+            return ret;
+        }
+
+        public StoreViewState Create(StoreViewType type)
+        {
+            if (mStack.Count > 0)
+            {
+                Peek().Items.ForEach(item => item.gameObject.SetActive(false));
+            }
+            var ret = new StoreViewState();
+            ret.Type = type;
+            mStack.Push(ret);
+            return ret;
+        }
+
+        public void Clear()
+        {
+            while (mStack.Count > 0)
+                Pop();
+        }
+    }
+
+    public class StoreController : MonoBehaviour, IStoreController
     {
         [Inject(Id = "BanGround")]
         private IStoreProvider store;
@@ -18,68 +89,137 @@ namespace Assets.Codes.Community
         public InputField SearchBar;
         public GameObject SongPrefab;
         public ScrollRect StoreRect;
+        public Text TitleText;
 
-        public int Offset { get; private set; }
         public const int LIMIT = 9;
 
-        private GridLayoutGroup StoreView;
-        private List<Song> mSongs = new List<Song>();
+        private StoreStack mViewStack;
         private GameObject mLoadingDisplay;
-        private bool isSearching;
-        private bool hasMorePages;
+        public GridLayoutGroup StoreView { get; private set; }
+        public bool isLoading { get; private set; }
 
-        private async UniTaskVoid Search(int offset = 0)
+        private void OnBackButtonClicked()
         {
-            isSearching = true;
-            store.Cancel();
-            Offset = offset;
-            if (offset == 0)
+            mViewStack.Pop();
+            if (mViewStack.Count == 0)
             {
-                mSongs.ForEach(song => Destroy(song.gameObject));
-                hasMorePages = true;
-                mSongs.Clear();
+                SceneLoader.LoadScene("Community", "Title");
             }
+            else
+            {
+                mViewStack.RefreshState();
+            }
+        }
+
+        public async UniTaskVoid LoadCharts(SongItem song, int offset)
+        {
+            isLoading = true;
+            store.Cancel();
+            var state = mViewStack.Create(StoreViewType.Chart);
+            state.SearchText = SearchBar.text;
+            state.Title = song.Title;
+            state.Song = song;
+
+            // Fetch from API
+            mLoadingDisplay.transform.SetAsLastSibling();
+            mLoadingDisplay.SetActive(true);
+
             try
             {
-                // Display loading
-                mLoadingDisplay.transform.SetAsLastSibling();
-                mLoadingDisplay.SetActive(true);
-                var songs = await store.Search(SearchBar.text, offset, LIMIT);
+                var charts = await store.GetCharts(song.Id, offset, LIMIT);
+
                 // Handle infinite scroll
-                if (songs.Count < LIMIT)
-                    hasMorePages = false;
-                Offset += songs.Count;
-                mLoadingDisplay.SetActive(false);
+                if (charts.Count < LIMIT)
+                    state.HasMorePages = false;
+                state.Offset += charts.Count;
+
                 // Create song items
-                foreach (var song in songs)
+                foreach (var chart in charts)
                 {
-                    var obj = Instantiate(SongPrefab, StoreView.transform).GetComponent<Song>();
-                    obj.SetSongItem(song);
-                    mSongs.Add(obj);
+                    var obj = Instantiate(SongPrefab, StoreView.transform).GetComponent<StoreItem>();
+                    obj.Controller = this;
+                    obj.SetItem(chart);
+                    state.Items.Add(obj);
                 }
             }
             catch (OperationCanceledException) { }
-            isSearching = false;
+
+            // Finish
+            mLoadingDisplay.SetActive(false);
+            mViewStack.RefreshState();
+            isLoading = false;
+        }
+
+        public async UniTaskVoid Search(string text, int offset = 0)
+        {
+            isLoading = true;
+            store.Cancel();
+            if (offset == 0)
+            {
+                mViewStack.Clear();
+                var state = mViewStack.Create(StoreViewType.Song);
+                state.SearchText = text;
+                state.Title = text.IsNullOrEmpty() ? "Community" : $"Search: {text}";
+                mViewStack.RefreshState();
+            }
+            try
+            {
+                var state = mViewStack.Peek();
+                state.Offset = offset;
+                // Display loading
+                mLoadingDisplay.transform.SetAsLastSibling();
+                mLoadingDisplay.SetActive(true);
+                var songs = await store.Search(text, offset, LIMIT);
+                // Handle infinite scroll
+                if (songs.Count < LIMIT)
+                    state.HasMorePages = false;
+                state.Offset += songs.Count;
+                // Create song items
+                foreach (var song in songs)
+                {
+                    var obj = Instantiate(SongPrefab, StoreView.transform).GetComponent<StoreItem>();
+                    obj.Controller = this;
+                    obj.SetItem(song);
+                    state.Items.Add(obj);
+                }
+            }
+            catch (OperationCanceledException) { }
+            mLoadingDisplay.SetActive(false);
+            isLoading = false;
         }
 
         void Start()
         {
+            mViewStack = new StoreStack(this);
+
             // Get loading display
             StoreView = StoreRect.viewport.GetComponentInChildren<GridLayoutGroup>();
             mLoadingDisplay = StoreView.transform.GetChild(0).gameObject;
             mLoadingDisplay.SetActive(false);
-            
+
             // Initial search
-            SearchBtn.onClick.AddListener(() => Search().Forget());
-            Search().Forget();
+            SearchBtn.onClick.AddListener(() => Search(SearchBar.text).Forget());
+            Search(SearchBar.text).Forget();
+
+            // Back button
+            BackBtn.onClick.AddListener(OnBackButtonClicked);
         }
 
         private void Update()
         {
-            if (hasMorePages && !isSearching && StoreRect.normalizedPosition.y < 0)
+            var state = mViewStack.Peek();
+            if (state == null)
+            {
+                return;
+            }
+            state.NormalizedY = StoreRect.normalizedPosition.y;
+            if (state.HasMorePages && !isLoading && state.NormalizedY < 0)
             {
                 // Infinite scroll
-                Search(Offset).Forget();
+                if (state.Type == StoreViewType.Song)
+                    Search(SearchBar.text, state.Offset).Forget();
+                else
+                    LoadCharts(state.Song, state.Offset).Forget();
             }
         }
 
--- Assets/Codes/Community/StoreItem.cs
+++ Assets/Codes/Community/StoreItem.cs
@@ -4,21 +4,32 @@ using Cysharp.Threading.Tasks;
 using System.Threading;
 using UnityEngine.Networking;
 using System;
+using Zenject;
 
 namespace BanGround.Community
 {
-    public class Song : MonoBehaviour
+    public class StoreItem : MonoBehaviour
     {
+        public IStoreController Controller;
+
         public Text Title;
         public Image Background;
         public Sprite DefaultImage;
 
         public SongItem SongItem { get; private set; }
+        public ChartItem ChartItem { get; private set; }
         private CancellationTokenSource mTokenSource = new CancellationTokenSource();
 
-        private void OnClick()
+        public void OnClick()
         {
-            Debug.Log(SongItem);
+            if (SongItem == null)
+            {
+                Debug.Log(ChartItem);
+            }
+            else
+            {
+                Controller.LoadCharts(SongItem, 0).Forget();
+            }
         }
 
         private void Start()
@@ -29,11 +40,12 @@ namespace BanGround.Community
         private async UniTaskVoid GetImage()
         {
             Background.sprite = DefaultImage;
-            if (string.IsNullOrEmpty(SongItem.BackgroundUrl))
+            string url = SongItem?.BackgroundUrl ?? ChartItem?.BackgroundUrl;
+            if (string.IsNullOrEmpty(url))
                 return;
             try
             {
-                using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(SongItem.BackgroundUrl))
+                using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(url))
                 {
                     await webRequest.SendWebRequest().WithCancellation(mTokenSource.Token);
                     if (webRequest.isHttpError || webRequest.isNetworkError)
@@ -46,11 +58,20 @@ namespace BanGround.Community
             catch (UnityWebRequestException) { }
         }
 
-        public void SetSongItem(SongItem item)
+        public void SetItem(SongItem item)
         {
+            ChartItem = null;
             SongItem = item;
             GetImage().Forget();
             Title.text = item.Title;
+        }
+
+        public void SetItem(ChartItem item)
+        {
+            SongItem = null;
+            ChartItem = item;
+            GetImage().Forget();
+            Title.text = "By " + item.Uploader.Nickname;
         }
 
         public void Cancel()
