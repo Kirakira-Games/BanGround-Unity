d4ef5c3304d22934fd9a8ccf050d4de4e286d4f2
KCFindstr <changyuz@usc.edu>
2020-11-19 01:04:30 -0800

    UI Fix in import and drawer. Error msg fix for FmodAudioProvider.

--- Assets/Codes/Audio/FmodAudioProvider.cs
+++ Assets/Codes/Audio/FmodAudioProvider.cs
@@ -16,8 +16,12 @@ namespace AudioProvider
         internal static void ErrCheck(RESULT result)
         {
             if (result != RESULT.OK)
-                //throw new Exception($"FMOD Error! Result code: {Enum.GetName(typeof(RESULT), result)}");
-                messageBannerController.ShowMsg(LogLevel.ERROR, Enum.GetName(typeof(RESULT), result), false);
+            {
+                if (messageBannerController == null)
+                    UnityEngine.Debug.LogError($"FMOD Error! Result code: {result}");
+                else
+                    messageBannerController.ShowMsg(LogLevel.ERROR, result.ToString(), false);
+            }
         }
     }
 
