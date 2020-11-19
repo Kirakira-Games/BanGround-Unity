441a09a3ef0479c85c4931983094ea936e5a11f2
KCFindstr <changyuz@usc.edu>
2020-11-18 16:55:10 -0800

    Add Tab Toggle Group Component

---
+++ Assets/Codes/Select/AdvanceUIComponent/TabToggleGroup.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Events;
+
+[Serializable]
+public class TabToggleGroupPanel
+{
+    public Toggle Toggle;
+    public RectTransform Target;
+}
+
+public class TabToggleGroup : MonoBehaviour
+{
+    public RectTransform Content;
+    public ScrollRect Scroll;
+    public TabToggleGroupPanel[] Tabs;
+
+    public int ActiveTabIndex { get; private set; }
+    public TabToggleGroupPanel ActiveTab => Tabs[ActiveTabIndex];
+
+    /// <summary>
+    /// Returns the top position of a panel. Must call <see cref="Canvas.ForceUpdateCanvases()"/> before calling this function.
+    /// </summary>
+    private Vector2 GetTopPosition(RectTransform panel)
+    {
+        Vector2 viewportLocalPosition = Scroll.viewport.localPosition;
+        Vector2 childLocalPosition = panel.localPosition;
+        return new Vector2(
+            0,
+            0 - (viewportLocalPosition.y + childLocalPosition.y)
+        );
+    }
+
+    private int GetActiveTabIndex()
+    {
+        Canvas.ForceUpdateCanvases();
+
+        for (var i = 0; i < Tabs.Length; i++)
+        {
+            var tab = Tabs[i];
+            var pos = GetTopPosition(tab.Target);
+            if (Content.localPosition.y <= pos.y + tab.Target.sizeDelta.y - 0.1f)
+                return i;
+        }
+
+        return 0;
+    }
+
+    private void OnToggle(int index, bool isOn)
+    {
+        if (isOn)
+        {
+            if (index != ActiveTabIndex)
+            {
+                ActiveTabIndex = index;
+                Content.localPosition = GetTopPosition(ActiveTab.Target);
+            }
+        }
+    }
+
+    void Start()
+    {
+        ActiveTabIndex = GetActiveTabIndex();
+        for (int i = 0; i < Tabs.Length; i++)
+        {
+            int index = i;
+            Tabs[i].Toggle.onValueChanged.AddListener(isOn => OnToggle(index, isOn));
+        }
+    }
+
+    void Update()
+    {
+        int index = GetActiveTabIndex();
+        if (index != ActiveTabIndex)
+        {
+            ActiveTabIndex = index;
+            ActiveTab.Toggle.isOn = true;
+        }
+    }
+}
