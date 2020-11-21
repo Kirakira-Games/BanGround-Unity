e158bfa728a25a26a77943f39212aa513bb30e80
KCFindstr <792491320@qq.com>
2020-11-20 23:10:11 -0800

    Fix: 二重奏

---
+++ Assets/Editor/ForceLoadScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Editor
+{
+    public class ForceLoadScene : ScriptableObject
+    {
+        [MenuItem("BanGround/Force load Select scene")]
+        static void ForceLoadSelectScene()
+        {
+            ForceLoadSceneByName("Select");
+        }
+
+        static void ForceLoadSceneByName(string name)
+        {
+            SceneManager.LoadScene(name);
+        }
+    }
+}
