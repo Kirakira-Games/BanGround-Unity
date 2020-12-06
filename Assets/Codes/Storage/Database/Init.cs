64023c799614e3cb4349f08e72e71eb6154a0a04
KCFindstr <792491320@qq.com>
2020-12-05 19:35:25 -0800

    Configure MM

---
+++ Assets/Codes/Storage/Database/Init.cs
@@ -0,0 +1,24 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using UnityEngine;
+using BanGround.Database.Models;
+
+namespace BanGround.Database
+{
+    public static class Initializer
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void SetupMessagePackResolver()
+        {
+            /*
+            StaticCompositeResolver.Instance.Register(new[]{
+                MasterMemoryResolver.Instance, // set MasterMemory generated resolver
+                Generated.Instance,    // set MessagePack generated resolver
+                StandardResolver.Instance      // set default MessagePack resolver
+            });
+
+            var options = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
+            MessagePackSerializer.DefaultOptions = options;*/
+        }
+    }
+}
---
+++ Assets/Codes/Storage/Database/Models/RankItem.cs
@@ -0,0 +1,30 @@
+﻿using MasterMemory;
+using MessagePack;
+
+namespace BanGround.Database.RawModels
+{
+    [MemoryTable("Rank"), MessagePackObject(true)]
+    public class RankItem
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        [SecondaryKey(0), NonUnique]
+        [SecondaryKey(1, keyOrder: 1), NonUnique]
+        public int ChartId { get; set; }
+        [SecondaryKey(1, keyOrder: 1), NonUnique]
+        public Difficulty difficulty { get; set; }
+
+        [SecondaryKey(2), NonUnique]
+        public int MusicId { get; set; }
+
+        public float Acc { get; set; }
+
+        public Ranks Rank { get; set; }
+
+        [SecondaryKey(3), NonUnique]
+        public string ChartHash { get; set; }
+
+        public string ReplayFile { get; set; }
+    }
+}
