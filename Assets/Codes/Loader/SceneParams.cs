40bb15d0705af3ca0a71bd28623326c6021d7ec5
KCFindstr <792491320@qq.com>
2020-12-13 16:58:16 -0800

    Complete scene stack support: scene parameters

---
+++ Assets/Codes/Loader/SceneParams.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BanGround.Scene
+{
+    public class SceneParams : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
