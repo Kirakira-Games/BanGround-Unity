43db6c82f737c026a1559285f073dba7edbb1764
KCFindstr <792491320@qq.com>
2021-01-09 20:35:43 -0800

    Add initial migration base code

---
+++ Assets/Codes/Storage/Migrations/2021/KpakToFs2021_01_10.cs
@@ -0,0 +1,24 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
+using UnityEngine;
+using Zenject;
+
+namespace BanGround.Database.Migrations
+{
+    public class KpakToFs2021_01_10 : MigrationBase
+    {
+        [Inject]
+        private IFileSystem fs;
+
+        public override int Id => 1;
+        public override string Description => "Migrate kpak to local file system";
+
+        public override async UniTask<bool> Commit()
+        {
+            Progress = 0;
+            Debug.Assert(fs != null);
+            throw new System.NotImplementedException("TODO: GEEKiDoS");
+            Progress = 1;
+        }
+    }
+}
