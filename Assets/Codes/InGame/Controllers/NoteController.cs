3a0ce750f659da7f1616be4b6e93272dd81b91f6
KCFindstr <792491320@qq.com>
2021-03-25 22:09:56 -0700

    error CS0847: An array initializer of length '5' is expected

--- Assets/Codes/InGame/Controllers/NoteController.cs
+++ Assets/Codes/InGame/Controllers/NoteController.cs
@@ -442,7 +442,7 @@ public class NoteController : MonoBehaviour, INoteController
         }
 
         // Sound effects
-        soundEffects = new ISoundEffect[5]
+        soundEffects = new ISoundEffect[]
         {
             await audioManager.PrecacheInGameSE(resourceLoader.LoadSEResource<TextAsset>("perfect.wav").bytes),
             await audioManager.PrecacheInGameSE(resourceLoader.LoadSEResource<TextAsset>("great.wav").bytes),
