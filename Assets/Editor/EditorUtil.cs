43a337d1a6408fb25be9031499064f25fbb97aea
KCFindstr <792491320@qq.com>
2021-07-05 18:31:57 -0700

    Add editor util to find all raycast targets

---
+++ Assets/Editor/EditorUtil.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Editor {
+    internal static class EditorUtil {
+        private static IEnumerable<GameObject> DfsGameObjects(GameObject obj) {
+            yield return obj;
+            foreach (var child in obj.GetComponentsInChildren<Transform>(true)) {
+                if (child.gameObject == obj) {
+                    continue;
+                }
+                foreach (var childObj in DfsGameObjects(child.gameObject)) {
+                    yield return childObj;
+                }
+            }
+        }
+        public static IEnumerable<GameObject> AllGameObjectsInScene(Scene scene) {
+            foreach (var obj in scene.GetRootGameObjects()) {
+                foreach (var ret in DfsGameObjects(obj)) {
+                    yield return ret;
+                }
+            }
+        }
+    }
+}
