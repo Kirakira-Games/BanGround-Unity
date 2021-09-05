d1a902600dbb87072ba97e663bd6f157dd37c13a
KCFindstr <792491320@qq.com>
2021-09-04 20:26:16 -0700

    Add API to fetch remote KVars

---
+++ Assets/Codes/Web/Profile.cs
@@ -0,0 +1,17 @@
+﻿namespace BanGround.Web.Profile
+{
+    public class RemoteKVarData
+    {
+        public int rm_data_version;
+        public string rm_ver_stable;
+        public string rm_ver_min;
+    }
+
+    public static class Extension
+    {
+        public static KiraWebRequest.Builder<RemoteKVarData> GetRemoteKVars(this IKiraWebRequest web)
+        {
+            return web.New<RemoteKVarData>().Get("profile/unity_vars");
+        }
+    }
+}
